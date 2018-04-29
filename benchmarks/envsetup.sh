fb154a891b9fe98cb9e186bcddfaff190393ff6d
Umur <darbaz2@illinois.edu>
2018-04-28 20:06:32 -0500

    modifications to urlparser linking

--- benchmarks/envsetup.sh
+++ benchmarks/envsetup.sh
@@ -3,35 +3,34 @@
 BUILD_DIR=$(pwd)/../libs
 
 # UNCOMMENT FOR UNIX
-# sudo apt-get install \
-#     g++ \
-#     cmake \
-#     libboost-all-dev \
-#     libevent-dev \
-#     libdouble-conversion-dev \
-#     libgoogle-glog-dev \
-#     libgflags-dev \
-#     libiberty-dev \
-#     liblz4-dev \
-#     liblzma-dev \
-#     libsnappy-dev \
-#     make \
-#     zlib1g-dev \
-#     binutils-dev \
-#     libjemalloc-dev \
-#     libssl-dev \
-#     pkg-config
+sudo apt-get install \
+    g++ \
+    cmake \
+    libboost-all-dev \
+    libevent-dev \
+    libdouble-conversion-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libiberty-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libsnappy-dev \
+    make \
+    zlib1g-dev \
+    binutils-dev \
+    libjemalloc-dev \
+    libssl-dev \
+    pkg-config
 
-# Get folly library and build it
-mkdir $BUILD_DIR
+# Untar the Folly library and move to build dir
+mkdir $BUILD_DIR 
 cd $BUILD_DIR
-wget https://github.com/facebook/folly/archive/v2018.04.23.00.tar.gz
+tar xzvf v2018.04.23.00.tar.gz && mv folly-2018.04.23.00 folly
+cd folly
+mkdir _build
+cd _build
 
-# Untar and move
-tar xzvf v2018.04.23.00.tar.gz
-mv folly-2018.04.23.00 folly
-
-mkdir _build && cd _build
+# Make the Folly library
 cmake configure ..
 make -j $(nproc)
-make install
+sudo make install
--- benchmarks/urlParser/parser.cpp
+++ benchmarks/urlParser/parser.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <stdlib.h>
+#include <boost/regex.hpp>
+#include <folly/Uri.h>
 
-int main()
+#define ARGCOUNT 2
+
+using namespace std;
+
+int main(int argc, char** argv)
 {
-	
+	if(argc != ARGCOUNT)
+	{
+		cout << "Error: Only 1 argument must be provided." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	folly::Uri folly("https://facebook.com/asdsdf/17312647.html");
+	folly.scheme();
+	folly.host();
+	folly.path();
 }
