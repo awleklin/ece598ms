ae961baca53b11a1d618c9ec439905330934aeee
Alexandra Wleklinski <wleklin2@linux-19.ews.illinois.edu>
2018-04-29 17:24:42 -0500

    Fitness tracking homebrew app added with readme and make shell script

---
+++ benchmarks/fitnessTrack/README.txt
@@ -0,0 +1,2 @@
+Make with the command "./make.sh"
+Run with the command "./a.out"
---
+++ benchmarks/fitnessTrack/fitnessTrack.c
@@ -0,0 +1,63 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "time.h"
+#include <gsl/gsl_histogram.h>
+#define minBPM 0
+#define maxBPM 220
+#define binsBPM 220
+#define maxBeatOffset 100
+#define sampleFreq 100
+#define intervalBPM 60
+#define runTime 120
+#define trigger 1000
+
+int main()
+{
+    volatile int x,second;
+    float bpm[runTime-intervalBPM] = {0};
+    int beats[runTime*sampleFreq];
+   
+    // Loop through seconds for given run time
+    for(second=0;second<runTime;second++){
+    
+        // Start timing
+        int msec = 0;
+        clock_t start = clock();
+        
+        gsl_histogram * h = gsl_histogram_alloc (binsBPM);
+        gsl_histogram_set_ranges_uniform (h, minBPM, maxBPM);
+        // Randomly generates a 1 at ~60 1's/minute
+        for(x=0;x<sampleFreq;x++){
+            if((rand()%sampleFreq)>(sampleFreq-2))
+                beats[second*sampleFreq+x] = 1;
+            else
+                beats[second*sampleFreq+x] = 0;
+        }
+        
+        // Begin calculating BPM
+        if(second>=intervalBPM){
+            for(x=0;x<(intervalBPM*sampleFreq);x++){
+                bpm[second-intervalBPM] +=  (float)beats[second*sampleFreq-x];
+            }
+            bpm[second-intervalBPM] *= (60/(float)intervalBPM);
+            // Populates the histogram structure
+            for(x=0;x<(second-intervalBPM);x++){
+                gsl_histogram_increment (h, bpm[x]);
+            }    
+        }
+        
+        if(second==(runTime-1)){
+            // Prints the histogram
+            gsl_histogram_fprintf (stdout, h, "%g", "%g");
+        }
+        gsl_histogram_free (h);
+        
+        // Read the clock and wait if necessary
+        while(msec < trigger){
+            clock_t difference = clock() - start;
+            msec = difference * 1000 / CLOCKS_PER_SEC;
+       }
+    }
+
+    return 0;
+}
